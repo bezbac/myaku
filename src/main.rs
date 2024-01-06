cf15412cb1e1b9360001bdd0f835a7930d16f124
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2024-01-06 16:01:36 +0100

    Implement collecting commits

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use anyhow::Result;
 use auth_git2::GitAuthenticator;
 use clap::{Parser, Subcommand};
 use env_logger::Env;
+use git2::{Oid, Repository, Signature, Sort};
 use log::info;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::{self, File};
-use std::io::{BufReader, Read};
+use std::io::{BufReader, Read, Write};
+use std::path::Path;
 use std::path::PathBuf;
 use tempdir::TempDir;
 
@@ -77,6 +79,52 @@ fn get_repository_name_from_url(url: &str) -> String {
     caps["main"].to_string()
 }
 
+#[derive(Serialize, Debug)]
+struct Author {
+    name: Option<String>,
+    email: Option<String>,
+}
+
+#[derive(Serialize, Debug)]
+struct CommitInfo {
+    id: String,
+    author: Author,
+    committer: Author,
+    message: Option<String>,
+    time: i64,
+}
+
+impl<'a> From<Signature<'a>> for Author {
+    fn from(item: Signature) -> Self {
+        Author {
+            name: item.name().map(|v| v.to_string()),
+            email: item.email().map(|v| v.to_string()),
+        }
+    }
+}
+
+fn get_commits(repo: &Repository) -> Result<Vec<CommitInfo>> {
+    let mut revwalk = repo.revwalk().unwrap();
+
+    revwalk.set_sorting(Sort::NONE)?;
+    revwalk.push_head()?;
+
+    let mut commits: Vec<_> = Vec::new();
+    for id in revwalk {
+        let oid = id?;
+        let commit = repo.find_commit(oid)?;
+        commits.push(CommitInfo {
+            id: commit.id().to_string(),
+            author: commit.author().into(),
+            committer: commit.committer().into(),
+            message: commit.message().map(|v| v.to_string()),
+            time: commit.time().seconds(),
+        })
+    }
+
+    Ok(commits)
+}
+
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
@@ -119,6 +167,18 @@ fn main() -> Result<()> {
             let repo = repo_builder.clone(&config.reference.url, tempdir.path())?;
 
             info!("Successfully cloned repository");
+
+            info!("Collecting commit information");
+
+            let commits = get_commits(&repo)?;
+
+            let output_dir = Path::new(".myaku");
+
+            fs::create_dir_all(output_dir)?;
+
+            let mut commits_file = File::create(output_dir.join("commits.json"))?;
+            let commits_file_content = serde_json::to_string(&commits)?;
+            commits_file.write_all(commits_file_content.as_bytes())?;
         }
         None => {}
     }
