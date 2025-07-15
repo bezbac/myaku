4d83873aba5056156b33d6b903e49930c406142d
Ben Bachem <10088265+bezbac@users.noreply.github.com>
2025-07-15 08:23:03 +0200

    wip

--- github/overview/script.ts
+++ github/overview/script.ts
@@ -4,19 +4,52 @@ import { JSDOM } from "npm:jsdom";
 
 const df = pl.readParquet("./.myaku/output/bezbac/myaku/commits.parquet");
 
-const dateColumn = df.getColumn("time").cast(pl.Date);
-const dfWithDate = df.withColumn(dateColumn);
+const dateValues = [...df.getColumn("time")].map((time) => {
+  const date = new Date(time * 1000);
+  return date.toISOString().split("T")[0];
+});
 
-// TODO: Properly group by date
-const groupedDf = dfWithDate.groupBy("date");
+const dateColumn = pl.Series("date", dateValues);
 
-const records = df.toRecords();
+const records = df
+  .withColumn(dateColumn)
+  .groupBy("date")
+  .agg(pl.col("date").count().alias("count"))
+  .sort("date")
+  .toRecords();
+
+const firstDate = Math.min(
+  ...records.map((r) => new Date(r.date as string).getTime())
+);
+const lastDate = Math.max(
+  ...records.map((r) => new Date(r.date as string).getTime())
+);
+
+const days = Math.ceil((lastDate - firstDate) / (1000 * 60 * 60 * 24));
+
+const normalized = Array.from({ length: days }, (_, i) => {
+  const date = new Date(firstDate + i * 1000 * 60 * 60 * 24)
+    .toISOString()
+    .split("T")[0];
+
+  const record = records.find((r) => r.date === date);
+
+  return {
+    date: new Date(date),
+    count: record ? record.count : 0,
+  };
+});
 
 const plot = Plot.plot({
   document: new JSDOM("").window.document,
   width: 900,
   marginLeft: 50,
-  marks: [Plot.barY(records, Plot.groupX())],
+  marks: [
+    Plot.barY(normalized, {
+      x: "date",
+      y: "count",
+    }),
+  ],
 });
 
 plot.setAttributeNS(
@@ -30,4 +63,4 @@ plot.setAttributeNS(
   "http://www.w3.org/1999/xlink"
 );
 
-process.stdout.write(plot.outerHTML);
+console.log(plot.outerHTML);
